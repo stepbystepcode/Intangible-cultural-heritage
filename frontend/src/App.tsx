baae019554a81d74588cf6a4f7d43800a585e184
Jing Li <abc1870819884@gmail.com>
2024-11-06 22:07:45 +0800

    use rsbuild

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
-function App() {
-
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+const App = () => {
   return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </>
-  )
-}
+    <div className="content">
+  <MapContainer
+      className="markercluster-map"
+      center={[51.0, 19.0]}
+      zoom={4}
+      maxZoom={18}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
 
-export default App
+    </MapContainer>
+    </div>
+  );
+};
+
+export default App;
